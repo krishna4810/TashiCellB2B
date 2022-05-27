d10a8fbc7b21fc0be4e5d9a81f036cf2356c3f94
krishna4810 <krishnaprasadsharma84@gmail.com>
2022-05-27 17:47:14 +0600

    feat: made image magnification on hover

--- src/home/components/product-detail/product-detail.component.ts
+++ src/home/components/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Form, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  myGroup = new FormGroup({
+    quantity: new FormControl(0)
+  });
+  disableOrderButton: boolean = false;
+  myThumbnail="../../../assets/download.jpeg";
+  myFullresImage="../../../assets/download.jpeg";
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.disableOrderButton = true;
+  }
+
+  incrementValue() {
+      this.disableOrderButton = false;
+      this.quantity.setValue(this.quantity.value+ 1);
+  }
+
+  decrementValue() {
+   this.quantity.value > 0 && this.quantity.setValue(this.quantity.value - 1);
+   this.quantity.value === 0 && (this.disableOrderButton = true);
+  }
+
+  get quantity(): FormControl {
+    return this.myGroup.controls['quantity'] as FormControl;
+  }
 }
